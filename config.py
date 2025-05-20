7172eb4ca217e06fba17360e8e739bbf12b0613b
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-05-20 08:16:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "3737117"))
+API_HASH = getenv("API_HASH", "f466ca6ff400954d192ce0992ddf8b5d")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 AUTH_CODE = getenv("AUTH_CODE", "") # eg: eyfgobtiOtrtrUzI......eyJpc3MiHRwewczovL3Rlc3Rib....
